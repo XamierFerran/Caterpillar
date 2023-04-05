5a619de13694de41aaeeef294621acb48709d1f2
Xamier <xamier.xf.ferran@gmail.com>
2023-04-05 12:34:24 -0400

    added to ESP code, and created mqtt_CBR.py file to import to esp

--- Vision/ESP.py
+++ Vision/ESP.py
@@ -1,4 +1,8 @@
-# import Chris's serial library
-# setup serial connection
+import time
+from machine import UART
+import Serial
+
+s = Serial.serial_comm(115200)
+
 # read in serial data
 # publish to MQTT (use temp values)
